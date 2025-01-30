69b617405caf7d1c9365396afedf58f0d5623ca3
yonatan-h <yon019hl@gmail.com>
2025-01-30 14:34:20 +0300

    half build ble test

---
+++ app/src/test/java/com/example/ble_dummy/ExampleUnitTest.kt
@@ -0,0 +1,28 @@
+package com.example.ble_dummy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    //test no error at start
+    //test near by devices from central
+    //test near by devices from peripheral
+    //test same device connecting from central and peripheral
+    //test sending as central
+    //test sending as peripheral
+    //test peripheral disconnecting
+    //test central disconnecting
+    //test stop
+    //test start then test listening to neighbor
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
