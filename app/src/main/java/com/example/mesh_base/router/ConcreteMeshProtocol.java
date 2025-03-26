73d57527bbb2e205c57c48b501493f2e2e5c01e7
primequantuM4 <gediezra@gmail.com>
2025-03-26 13:43:46 +0300

    feat: Initial routing protocol with encode and decode

---
+++ app/src/main/java/com/example/mesh_base/router/ConcreteMeshProtocol.java
@@ -0,0 +1,9 @@
+package com.example.mesh_base.router;
+
+import java.util.UUID;
+
+public class ConcreteMeshProtocol<T extends MeshSerializer<T>> extends MeshProtocol<T>{
+    public ConcreteMeshProtocol(int messageType, int remainingHops, int messageId, UUID sender, T body) {
+        super(messageType, remainingHops, messageId, sender, body);
+    }
+}
