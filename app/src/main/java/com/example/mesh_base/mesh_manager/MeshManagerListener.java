98f870953eb3182029e3365f094b7f4937dd2cbe
yonatan-h <yon019hl@gmail.com>
2025-04-07 18:45:12 +0300

    implement neighbors changed listener

--- app/src/main/java/com/example/mesh_base/mesh_manager/MeshManagerListener.java
+++ app/src/main/java/com/example/mesh_base/mesh_manager/MeshManagerListener.java
@@ -1,10 +1,14 @@
 package com.example.mesh_base.mesh_manager;
 
+import com.example.mesh_base.global_interfaces.Device;
+
+import java.util.ArrayList;
+
 public interface MeshManagerListener {
   void onData(byte[] data);
 
   void onStatusChange(Status status);
 
   //TODO: add medium type in devices
-  void onNeighborsChanged();
+  void onNeighborsChanged(ArrayList<Device> devices);
 }
