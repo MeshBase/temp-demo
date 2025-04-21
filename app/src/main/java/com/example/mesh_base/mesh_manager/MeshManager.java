a0cd1f40fb2aff324a801eddc1301dc07474f572
yonatan-h <yon019hl@gmail.com>
2025-04-21 07:34:58 +0300

    connect start() and stop() to onconnection() and onDisconnect() results

--- app/src/main/java/com/example/mesh_base/mesh_manager/MeshManager.java
+++ app/src/main/java/com/example/mesh_base/mesh_manager/MeshManager.java
@@ -17,112 +17,111 @@ import java.util.UUID;
 
 //TODO: unit test once BLE and WifiDirect have the same interfaces
 public class MeshManager {
-    private final HashMap<ConnectionHandlersEnum, ConnectionHandler> connectionHandlers = new HashMap<>();
-    //TODO: store uuid in local storage so that the devices address is consistent
-    private final UUID id;
-    private final Router router;
-    String TAG = "my_meshManager";
-    private boolean isOn = false;
-    private MeshManagerListener listener = MeshManagerListener.createEmpty();
+  private final HashMap<ConnectionHandlersEnum, ConnectionHandler> connectionHandlers = new HashMap<>();
+  //TODO: store uuid in local storage so that the devices address is consistent
+  private final UUID id;
+  private final Router router;
+  String TAG = "my_meshManager";
+  private boolean isOn = false;
+  private MeshManagerListener listener = MeshManagerListener.createEmpty();
 
-    public MeshManager(ComponentActivity context) {
-        Store store = Store.getInstance(context);
-        if (store.getId() == null) store.storeId(UUID.randomUUID());
-        id = store.getId();
+  public MeshManager(ComponentActivity context) {
+    Store store = Store.getInstance(context);
+    if (store.getId() == null) store.storeId(UUID.randomUUID());
+    id = store.getId();
 
-        ConnectionHandlerFactory factory = new ConnectionHandlerFactory();
+    ConnectionHandlerFactory factory = new ConnectionHandlerFactory();
 
-        // TODO: Check for user connection handler preference
-        for (ConnectionHandlersEnum _enum : ConnectionHandlersEnum.values()) {
-            ConnectionHandler connectionHandler = factory.createConnectionHandler(_enum, context, id);
-            connectionHandlers.put(_enum, connectionHandler);
-            connectionHandler.subscribe(
-                    new ConnectionHandlerListener() {
-                        @Override
-                        public void onNeighborConnected(Device device) {
-                            listener.onNeighborConnected(device);
-                        }
-
-                        @Override
-                        public void onNeighborDisconnected(Device device) {
-                            listener.onNeighborDisconnected(device);
-                        }
-
-                        @Override
-                        public void onDisconnected() {
-                            listener.onStatusChange(getStatus());
-                        }
-
-                        @Override
-                        public void onConnected() {
-                            // TODO: change status
-                            listener.onStatusChange(getStatus());
-                        }
-
-                        @Override
-                        public void onDataReceived(Device device, byte[] data) {
-                            if (device.uuid == id) {
-                                listener.onDataReceivedForSelf(data);
-                            }
-                        }
-                    }
-            );
-        }
-
-        router = new Router(connectionHandlers, id);
-        //TODO: accept protocol instead of byte array once the router's handleOnData is modified, to not cause conflict
-        //TODO: consider exposing the protocol itself to users
-        router.setOnReceivedData(((data, neighbor) -> {
-            listener.onDataReceivedForSelf(data);
-        }));
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setListener(MeshManagerListener listener) {
-        this.listener = listener;
-    }
-
-    public void on() {
-        for (ConnectionHandler connectionHandler : connectionHandlers.values()) {
-            connectionHandler.enable();
-        }
-        isOn = true;
-    }
-
-    public void off() {
-        for (ConnectionHandler helper : connectionHandlers.values()) {
-            helper.stop();
-        }
-        isOn = false;
-    }
-
-    public ArrayList<Device> getNeighbors() {
-        ArrayList<Device> neighbors = new ArrayList<Device>();
-        for (ConnectionHandler helper : connectionHandlers.values()) {
-            neighbors.addAll(helper.getNeighbourDevices());
-        }
-        return neighbors;
-    }
-
-    public Status getStatus() {
-        HashMap<ConnectionHandlersEnum, Status.Property> _status = new HashMap<>() {
-            {
-                for (Entry<ConnectionHandlersEnum, ConnectionHandler> helper : connectionHandlers.entrySet()) {
-                    ConnectionHandlersEnum key = helper.getKey();
-                    ConnectionHandler value = helper.getValue();
-                    // TODO: check status and is on and is allowed in the connection handlers
-                    put(key, new Status.Property(true, true, true));
+    // TODO: Check for user connection handler preference
+    for (ConnectionHandlersEnum _enum : ConnectionHandlersEnum.values()) {
+      ConnectionHandler connectionHandler = factory.createConnectionHandler(_enum, context, id);
+      connectionHandlers.put(_enum, connectionHandler);
+      connectionHandler.subscribe(
+              new ConnectionHandlerListener() {
+                @Override
+                public void onNeighborConnected(Device device) {
+                  listener.onNeighborConnected(device);
                 }
-            }
-        };
 
-        return new Status(isOn, _status);
+                @Override
+                public void onNeighborDisconnected(Device device) {
+                  listener.onNeighborDisconnected(device);
+                }
+
+                @Override
+                public void onDisconnected() {
+                  listener.onStatusChange(getStatus());
+                }
+
+                @Override
+                public void onConnected() {
+                  listener.onStatusChange(getStatus());
+                }
+
+                @Override
+                public void onDataReceived(Device device, byte[] data) {
+                  if (device.uuid == id) {
+                    listener.onDataReceivedForSelf(data);
+                  }
+                }
+              }
+      );
     }
 
-    public void send(MeshProtocol<?> protocol, SendListener listener) {
-        router.sendData(protocol, listener);
+    router = new Router(connectionHandlers, id);
+    //TODO: accept protocol instead of byte array once the router's handleOnData is modified, to not cause conflict
+    //TODO: consider exposing the protocol itself to users
+    router.setOnReceivedData(((data, neighbor) -> {
+      listener.onDataReceivedForSelf(data);
+    }));
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setListener(MeshManagerListener listener) {
+    this.listener = listener;
+  }
+
+  public void on() {
+    for (ConnectionHandler connectionHandler : connectionHandlers.values()) {
+      connectionHandler.enable();
     }
+    isOn = true;
+  }
+
+  public void off() {
+    for (ConnectionHandler helper : connectionHandlers.values()) {
+      helper.stop();
+    }
+    isOn = false;
+  }
+
+  public ArrayList<Device> getNeighbors() {
+    ArrayList<Device> neighbors = new ArrayList<Device>();
+    for (ConnectionHandler helper : connectionHandlers.values()) {
+      neighbors.addAll(helper.getNeighbourDevices());
+    }
+    return neighbors;
+  }
+
+  public Status getStatus() {
+    HashMap<ConnectionHandlersEnum, Status.Property> _status = new HashMap<>() {
+      {
+        for (Entry<ConnectionHandlersEnum, ConnectionHandler> helper : connectionHandlers.entrySet()) {
+          ConnectionHandlersEnum key = helper.getKey();
+          ConnectionHandler value = helper.getValue();
+          // TODO: check status and is on and is allowed in the connection handlers
+          put(key, new Status.Property(true, true, true));
+        }
+      }
+    };
+
+    return new Status(isOn, _status);
+  }
+
+  public void send(MeshProtocol<?> protocol, SendListener listener) {
+    router.sendData(protocol, listener);
+  }
 }
