cbde0a57a063abcf58a43b0b9d65683bf4489051
yonatan-h <yon019hl@gmail.com>
2025-04-21 07:18:58 +0300

    update listeners in ble test screen

--- app/src/main/java/com/example/mesh_base/mesh_manager/Status.java
+++ app/src/main/java/com/example/mesh_base/mesh_manager/Status.java
@@ -5,57 +5,53 @@ import com.example.mesh_base.global_interfaces.ConnectionHandlersEnum;
 import java.util.HashMap;
 
 public class Status {
+  private final boolean isOn;
+  private final HashMap<ConnectionHandlersEnum, Property> connectionStatuses;
+
+  Status(boolean isOn, HashMap<ConnectionHandlersEnum, Property> connections) {
+    this.isOn = isOn;
+    this.connectionStatuses = connections;
+  }
+
+  public HashMap<ConnectionHandlersEnum, Property> getStatus() {
+    return connectionStatuses;
+  }
+
+  public Property getStatus(ConnectionHandlersEnum connectionType) {
+    return connectionStatuses.get(connectionType);
+  }
+
+  public HashMap<ConnectionHandlersEnum, Property> getConnectionStatuses() {
+    return connectionStatuses;
+  }
+
+  public boolean isOn() {
+    return isOn;
+  }
+
+  public static class Property {
+
+    private final boolean isSupported;
     private final boolean isOn;
-    private final HashMap<ConnectionHandlersEnum, Property> connectionStatuses;
+    private final boolean isAllowed;
 
-    Status(boolean isOn, HashMap<ConnectionHandlersEnum, Property> connections) {
-        this.isOn = isOn;
-        this.connectionStatuses = connections;
+    public Property(boolean isSupported, boolean isOn, boolean isAllowed) {
+      this.isSupported = isSupported;
+      this.isOn = isOn;
+      this.isAllowed = isAllowed;
     }
 
-    public HashMap<ConnectionHandlersEnum, Property> getStatus() {
-        return connectionStatuses;
+    public boolean isSupported() {
+      return isSupported;
     }
-
-    public Property getStatus(ConnectionHandlersEnum connectionType) {
-        return connectionStatuses.get(connectionType);
-    }
-
-//    public Property getBle() {
-//        return ble;
-//    }
-//
-//    public Property getWifiDirect() {
-//        return wifiDirect;
-//    }
 
     public boolean isOn() {
-        return isOn;
+      return isOn;
     }
 
-    public static class Property {
-
-        private final boolean isSupported;
-        private final boolean isOn;
-        private final boolean isAllowed;
-
-        public Property(boolean isSupported, boolean isOn, boolean isAllowed) {
-            this.isSupported = isSupported;
-            this.isOn = isOn;
-            this.isAllowed = isAllowed;
-        }
-
-        public boolean isSupported() {
-            return isSupported;
-        }
-
-        public boolean isOn() {
-            return isOn;
-        }
-
-        public boolean isAllowed() {
-            return isAllowed;
-        }
+    public boolean isAllowed() {
+      return isAllowed;
     }
+  }
 
 }
